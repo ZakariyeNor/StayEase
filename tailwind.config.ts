80824b0138208eb7cabea0de736aeb4de9c2ed69
Zakariye <zakilenor@gmail.com>
2025-09-15 10:02:16 +0200

    customize the app

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,20 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      fontFamily: {
+        heading: ["Playfair Display", "serif"],
+        body: ["Open Sans", "sans-serif"],
+        accent: ["Barlow", "sans-serif"],
+      },
+      colors: {
+        primary: '#FFFFFF',
+        secondary: {
+          DEFAULT: '#1F1F1F', // Dark black
+          gray: '#4B5563',    // Gray tone
+        },
+        accent: '#0EA5E9', // Sky blue
+        mainText: '#000000',       // Main text
+        accentText: '#FFFFFF', // For text on accent buttons
       },
     },
   },
